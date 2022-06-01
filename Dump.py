d5840f0351fb45e51c70ba2d5d510415ae4a6262
Mr-Unknow-420 <104737376+Mr-Unknow-420@users.noreply.github.com>
2022-06-01 22:50:27 +0500

    Create Dump.py

---
+++ Dump.py
@@ -0,0 +1,16 @@
+import os, platform, time
+try:
+    import rich
+    os.system('pip insall rich')
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls\033[1;37m")
+    from Dump import Menu
+    Menu()
+elif bit == '32bit':
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
